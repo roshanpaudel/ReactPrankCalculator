0913109c2e7ddfc5c5ebb4d0a172272f0adc96ca
roshanpaudel <paudelroshan93@gmail.com>
2025-05-11 22:35:22 +1000

    HTML migrated from old JS based prank calculaator project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
 
 const App = () => {
-  return <>Hello World!</>;
+  return (
+    <div className="wrapper flex-center">
+      <div className="calculator">
+        <div className="display arbutus-regular">0.0</div>
+        <div className="btn btn-ac">AC</div>
+        <div className="btn btn-c">C</div>
+        <div className="btn btn-per">%</div>
+        <div className="btn btn-divide">/</div>
+        <div className="btn btn-7">7</div>
+        <div className="btn btn-8">8</div>
+        <div className="btn btn-9">9</div>
+        <div className="btn btn-x">*</div>
+        <div className="btn btn-4">4</div>
+        <div className="btn btn-5">5</div>
+        <div className="btn btn-6">6</div>
+        <div className="btn btn-minus">-</div>
+        <div className="btn btn-1">1</div>
+        <div className="btn btn-2">2</div>
+        <div className="btn btn-3">3</div>
+        <div className="btn btn-plus">+</div>
+        <div className="btn btn-0">0</div>
+        <div className="btn btn-dot">.</div>
+        <div className="btn btn-equal">=</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
